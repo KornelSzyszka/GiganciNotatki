27bacbd548f46a7b0e89f42e099b8c326d3aa919
KornelSzyszka <kszyszka13@gmail.com>
2023-11-29 17:22:39 +0100

    cpp-rand

---
+++ Cpp/Lekcja5.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+int main()
+{
+    const int LICZBA_MAKSYMALNA = 20;
+    const int LICZBA_MINIMALNA = 10;
+    // inicjacja generatora ziarna
+    srand(time(NULL));
+    // losowanie liczby z zakresu od 10 do 20
+    int liczbaPseudolosowa = rand()%(LICZBA_MAKSYMALNA - LICZBA_MINIMALNA + 1) + LICZBA_MINIMALNA;
+    // 70000 129
+    int drugaLiczba = rand() % (70000 - 129 + 1) + 129;
+                                // gorny zakres + dolny zakres
+    if (liczbaPseudolosowa > 15) {
+        std::cout << "Nasza liczba jest wieksza niz 15" << std::endl;
+        std::cout << "Rowna jest dokladnie: " << liczbaPseudolosowa;
+    }
+    else {
+        std::cout << "Nasza liczba jest mniejsza niz 15" << std::endl;
+        std::cout << "Rowna jest dokladnie: " << liczbaPseudolosowa;
+    }
+}
+
